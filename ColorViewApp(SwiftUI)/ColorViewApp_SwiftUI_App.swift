8023a0b30a7e2135e610d0a3de8e162453622052
Dmitri Gordeev <dimapushkin@gmail.com>
2021-10-30 20:36:20 +0300

    Initial Commit

---
+++ ColorViewApp(SwiftUI)/ColorViewApp_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewApp_SwiftUI_App.swift
+//  ColorViewApp(SwiftUI)
+//
+//  Created by Повелитель on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewApp_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
